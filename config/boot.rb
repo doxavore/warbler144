0d176c259d3d06493db3cdc9266b447cbe3bc9bb
Doug Mayer <dougtmayer@gmail.com>
2013-04-20 11:54:30 -0500

    Add "require bundler" note.

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,7 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+# Attempted to require 'bundler' in addition to 'bundler/setup',
+# but it doesn't seem to make a difference.
+#   require 'bundler' if File.exists?(ENV['BUNDLE_GEMFILE'])
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
