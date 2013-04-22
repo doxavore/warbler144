ddc0806db37e03bac4c58cee25882171dc0c4184
Doug Mayer <dougtmayer@gmail.com>
2013-04-21 19:55:33 -0500

    Fix by manually requiring Bundler autoloads.

--- config/boot.rb
+++ config/boot.rb
@@ -3,7 +3,21 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-# Attempted to require 'bundler' in addition to 'bundler/setup',
-# but it doesn't seem to make a difference.
-#   require 'bundler' if File.exists?(ENV['BUNDLE_GEMFILE'])
+# ALL of these are required for this project to run in the error scenario
+require 'bundler'
+require 'bundler/index'
+require 'bundler/remote_specification'
+require 'bundler/endpoint_specification'
+require 'bundler/dep_proxy'
+
+puts "\n----"
+puts "GEM_HOME: #{ENV['GEM_HOME']}"
+puts "Gem.dir: #{Gem.dir}"
+puts "Gem.path:\n#{Gem.path.join("\n")}"
+puts "Gemfile: #{ENV['BUNDLE_GEMFILE']}"
+puts "Bundler.bundle_path: #{Bundler.bundle_path}"
+puts "Bundler.root: #{Bundler.root}"
+puts "Load paths:\n#{$:.join("\n")}"
+puts "----\n\n"
+
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
